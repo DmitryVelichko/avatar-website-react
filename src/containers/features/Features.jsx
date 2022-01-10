bc04bd12c30b187ef264b601b63bb066442fd890
Dmitry Velichko <soleil-2005@mail.ru>
2022-01-10 23:31:57 +0300

    New commit

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -3,7 +3,11 @@ import './features.css'
 
 const Features = () => {
     return (
-    
+        <div className='gpt3__features section__padding' id='features'>
+            <div className='gpt3__features__heading'>
+                <h1 className="gradient-text"></h1>
+            </div>
+        </div>
     )
 }
 
