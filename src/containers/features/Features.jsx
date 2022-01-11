348967396187fbb0ba0357e7e326884db23e5bd9
Dmitry Velichko <soleil-2005@mail.ru>
2022-01-11 11:58:12 +0300

    New commit

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -28,7 +28,7 @@ const Features = () => {
         <div className='gpt3__features section__padding' id='features'>
             <div className='gpt3__features-heading'>
                 <h1 className="gradient__text">The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
-                <p>The possibilities are beyond your imagination</p>
+                <p>Request Early Access to Get Started</p>
             </div>
             <div className="gpt3__features-container">
                 {featuresData.map((item, index) => (
